b1ddf218d59d1c7c73e1d26b7638a5585ac8277e
unknown <chadrack.mayamba.7@gmail.com>
2025-07-14 16:29:38 +0200

    Configuration file.

---
+++ config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    "lr": 5e-5,
+    "batch_size": 8,
+    "epochs": 10,
+    "warmup_ratio": 0.1,
+    "weight_decay": 0.01,
+    "max_seq_length": 256,
+    "gradient_accumulation_steps": 4
+}
